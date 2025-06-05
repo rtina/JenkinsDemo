92a1a4f72203f3747d7b023801f5de8d25258a89
Tina <rtina2037@gmail.com>
2025-06-05 09:51:29 +0530

    Polling Test

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ package JenkinsDemo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Jenkins Polling Mechanism Test");
     }
 }
