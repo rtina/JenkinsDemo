ad2fc79abba8dd8d67057839ca3ddf0ce9f96213
Tina <rtina2037@gmail.com>
2025-06-05 10:08:33 +0530

    helloworld file changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Jenkins Polling Mechanism Test");
+         System.out.println("pom.xml file added");
     }
 }
